37797cbfd6c03859817d69c660dbb3fff60bf3d5
Arufff05 <145822930+Arufff05@users.noreply.github.com>
2023-09-23 14:48:33 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+print("1: Add\n2: Subtract\n3: Multiply\n4: Divide")
+c = int(input("Enter choice: "))
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+if(c==1):
+    print(a+b)
+elif c==2:
+    if(a>b):
+        print(a-b)
+    else:
+        print(b-a)
+elif c==3:
+    print(a*b)
+else:
+    print(float)(a/b)
